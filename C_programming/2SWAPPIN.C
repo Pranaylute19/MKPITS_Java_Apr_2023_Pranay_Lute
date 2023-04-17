975b6c352867d2a80b82da820be56af4bcdbd764
unknown <pranaylute1@gmail.com>
2023-04-17 14:31:49 +0530

    Addition

---
+++ C_programming/2SWAPPIN.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+int a,b,c,d;
+printf("enter the value ");
+c=a;
+a=b;
+b=c;
+scanf("%d%d",&a,&b);
+printf("This is the value of a  %d",a);
+printf("this is the value of b  %d",b);
+
+d=a;
+a=b;
+printf("this is the value of a  %d ",b);
+printf("this is the value of b  %d ",d);
+getch();
+}
+
+
+
