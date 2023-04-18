4105edf09a207dfa08446009b84afabc645c6bf3
Pranaylute19 <pranaylute1@gmail.com>
2023-04-18 21:05:38 +0530

    salary

---
+++ C_programming/SALARY.C
@@ -0,0 +1,13 @@
+//write a C program that accept an employeeIDs totl worked hours of month and the amount he received per hous. print the employee id and salary of a particular month.
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+clrscr();
+int id,hour,value,salary;
+printf("enter the value");
+scanf("%d%d",&id,&value);
+salary=hour*value;
+printf("%d",salary);
+getch();
+}
