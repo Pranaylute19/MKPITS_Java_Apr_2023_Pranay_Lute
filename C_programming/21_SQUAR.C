3ab5575f8e4250462ecd4484318bf5a176336514
Pranaylute19 <pranaylute1@gmail.com>
2023-04-23 17:35:36 +0530

    22 april

---
+++ C_programming/21_SQUAR.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+int num1,res;
+printf("enter the side of square");
+scanf("%d",&num1);
+res=num1%2;
+if(res!=0){
+printf("you can create square");
+}
+else{
+printf("you cannot create square");
+
+     }
+getch();}
