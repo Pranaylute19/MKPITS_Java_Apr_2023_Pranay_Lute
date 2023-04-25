62c301ccbafd841a0e374d19d1cddb5972539d37
Pranaylute19 <123226838+Pranaylute19@users.noreply.github.com>
2023-04-25 10:45:45 +0530

    Update 24_SER.C

--- C_programming/24_SER.C
+++ C_programming/24_SER.C
@@ -4,7 +4,7 @@ void main(){
 int num1,num2;
 for(num1=1;num1<=5;num1++){
       for(num2=1;num2<=4;num2++){
-      printf("%d",num2);
+      printf("%d",num1);
       }
       printf("\n");
       }
