1086631a340749b924926ec11e688d1d2e8067a9
Pranaylute19 <pranaylute1@gmail.com>
2023-05-08 17:31:09 +0530

    8 may

---
+++ C_programming/TRI.C
@@ -0,0 +1,16 @@
+//no argumnt with no return type
+#include<stdio.h>
+#include<conio.h>
+void funt(){
+    int l,b,res;
+    scanf("%f%f",&l,&b);
+     res=0.5*l*b;
+     printf("%f",res);
+     }
+     void main(){
+     funt();
+     getch();
+     }
+
+
+
