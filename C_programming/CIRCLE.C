8111c584b74e492d723835abe4153321240883fc
Pranaylute19 <pranaylute1@gmail.com>
2023-05-08 17:25:02 +0530

    8 may

---
+++ C_programming/CIRCLE.C
@@ -0,0 +1,16 @@
+//no argumnt with no return type
+#include<stdio.h>
+#include<conio.h>
+void funt(){
+    int pi=3.14,r,res;
+    scanf("%d",&r);
+     res=pi*r*r;
+     printf("%d",res);
+     }
+     void main(){
+     funt();
+     getch();
+     }
+
+
+
