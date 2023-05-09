856ab465b575f8e4679c1067f14ba0218c9a8422
Pranaylute19 <pranaylute1@gmail.com>
2023-05-09 17:23:30 +0530

    9 may

--- C_programming/2_DATA.C
+++ C_programming/2_DATA.C
@@ -2,6 +2,6 @@
 #include<conio.h>
 void main(){
  clrscr();
- printf("Name : Pranay lute \nDOB: 19/07/1998 \n Mobile: 7769935097 \n");
+ printf("Name : Pranay lute\nDOB: 19/07/1998 \n Mobile: 7769935097 \n");
  getch();
  }
