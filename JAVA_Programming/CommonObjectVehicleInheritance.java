8e606657701c51e0b04c9f8085975de775b30f90
Pranaylute19 <pranaylute1@gmail.com>
2023-06-15 14:38:41 +0530

    14/6

---
+++ JAVA_Programming/CommonObjectVehicleInheritance.java
@@ -0,0 +1,27 @@
+Class CommonObjectVehicle{
+String Vehiclenumber;
+String fuel ;
+String owner;
+integer NoOfTyre;
+}
+Class vehicle {
+List< commonObject> commonObject;
+String TypeofVehicle;
+String typeoftyre;
+Integer No.ofseat;
+}
+Class 2w extend commonObject {
+   String handleWithAccelatorAndbrake;
+   string Typeof2-w;
+   integer mileage;
+}
+Class 3-w extend commonObject{
+string Typeof3-wVehicle;
+String handleType;
+String permit;
+}
+Class 4-w extend commonObject{
+String WheelSteering
+String Typeof4-w;
+String FuelType
+}
