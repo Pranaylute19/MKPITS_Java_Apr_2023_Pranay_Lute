15ac0c7f1b153a5c32258600a49922718920bca7
Pranaylute19 <pranaylute1@gmail.com>
2023-06-15 14:49:30 +0530

    14/6

---
+++ JAVA_Programming/CollegeInheritance.java
@@ -0,0 +1,20 @@
+class college{
+	string uniform;
+	string department;
+	string human;
+	}
+	class externalManagement extend college{
+	string pariking;
+	string watchman;
+	string housekeeping;
+	}
+	class Teachingstaff extend College{
+	list<Teacher>teacher;
+	string HOD;
+	String Supervisor;
+	}
+	class student extend college{
+	string standards;
+	String Sections;
+	}
+	
