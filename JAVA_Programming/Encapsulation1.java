28225a522ed4152b63074ad1a71f8445c96151fd
Pranaylute19 <pranaylute1@gmail.com>
2023-06-24 10:42:18 +0530

    encapsulation

---
+++ JAVA_Programming/Encapsulation1.java
@@ -0,0 +1,11 @@
+class Encapsulation1{
+public static void main(String[] args){
+
+Encapsulation encapsulation = new Encapsulation();
+encapsulation.setName("Pranay");
+encapsulation.setAge(22);
+
+System.out.println(encapsulation.getName()+" "+encapsulation.getAge());
+
+}
+}
