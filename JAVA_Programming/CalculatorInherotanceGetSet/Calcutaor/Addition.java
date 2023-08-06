6d2968241729e4b7b44b640ce63d3efa0258b3e7
Pranaylute19 <pranaylute1@gmail.com>
2023-08-06 22:34:26 +0530

    shapegettersetter

---
+++ JAVA_Programming/CalculatorInherotanceGetSet/Calcutaor/Addition.java
@@ -0,0 +1,9 @@
+package Calcutaor;
+
+public class Addition extends CommonAbstractMethod {
+
+    @Override
+    public int calci(int a, int b) {
+        return a+b;
+    }
+}
---
+++ JAVA_Programming/CalculatorInherotanceGetSet/Calcutaor/Multiplication.java
@@ -0,0 +1,7 @@
+package Calcutaor;
+
+public class Multiplication extends CommonAbstractMethod {
+    public int calci(int a, int b){
+        return a*b;
+    }
+}
