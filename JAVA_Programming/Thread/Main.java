805f0bee8b4df60971b1310ba89c4f2fc0d06a5b
Pranaylute19 <pranaylute1@gmail.com>
2023-08-20 14:26:37 +0530

    Threads

--- JAVA_Programming/Thread/Main.java
+++ JAVA_Programming/Thread/Main.java
@@ -1,31 +1,64 @@
-import EvenOdd.Even;
-import EvenOdd.Odd;
-import RandomNumber.RandomNumber;
-import RandomNumber.RandomMethod1;
+import RepetedClassname.mythread;
+import Threads.Threads;
 import Threads.Yourthread;
+import messageSender.ThrededSend;
+import messageSender.sender;
+import myWaitMethodThread.*;
+import myWaitMethodThread.myMethodWait2;
 
 public class Main {
-    public static void main(String[] args)  {
-
-//      Thread t1=new Thread(new Even());
-//      Thread t2=new Thread(new Odd());
-//      t1.start();
-//      t2.start();
-
-       Thread t1=new Thread(new RandomNumber());
-       Thread t2=new Thread(new RandomMethod1());
-       t1.start();
-       t2.start();
+    public static void main(String[] args) throws InterruptedException {
 
 
-        Thread t1=new Thread(new Yourthread(),"Shahrukh");
-        t1.setPriority(1);
-        Thread t=new Thread(new Yourthread(),"Salman");
-        t.setPriority(10);
-        t.getPriority();
-        t1.start();
-        t1.getPriority();
+//            Thread t=new Thread(new mythread());
+//
+//            Thread t1=new Thread(new mythread());
+//        t1.start();
+//            t.join();
+//        t.start();
+
+//        sender obj=new sender();
+//        ThrededSend t=new ThrededSend("hello  ",obj);
+//        ThrededSend t1=new ThrededSend("byy  ",obj);
+//        t.start();
+//        t1.start();
+
+
+//        Thread t1=new Thread(new mymethodWait());
+        mymethodWait t = new mymethodWait();
         t.start();
+
+        synchronized (t){
+            t.wait();
+        }
+
+
+
+
+
+//        System.out.println("hii");
+
+
+//        Thread t1=new Thread(new Yourthread(),"Shahrukh");
+//        t1.setPriority(1);
+//        Thread t=new Thread(new Yourthread(),"Salman");
+//        t.setPriority(10);
+//        t.getPriority();
+//        t1.start();
+//    t1.getPriority();
+//        t.start();
+
+
+//        System.out.println(t.getPriority());
+//        System.out.println(Thread.activeCount());
+//        System.out.println(t.getId());
+
+//        Thread th=new Threads("Salman");
+//        th.setName("SRK");
+//        th.start();
+//        System.out.println(th.getPriority());
+//        System.out.println("bye");
+//        System.out.println(th.getName());
 
     }
 }
