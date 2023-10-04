812516c30ec415baf9b6efd153f0a05a186f19ae
Pranaylute19 <pranaylute1@gmail.com>
2023-10-04 14:08:59 +0530

    Reverse String practise

---
+++ JAVA_Programming/ReverseString/Main.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("1 - Insert the data \n 2- display the data");
+        Scanner sc =new Scanner(System.in);
+
+
+        int count=sc.nextInt();
+
+
+            switch (count){
+
+            case 1:
+                Database.insertData("Abhi","Dangore","nanded");
+                break;
+
+            case 2:
+                Database.displayData();
+
+
+        }
+
+
+    }
+    }
