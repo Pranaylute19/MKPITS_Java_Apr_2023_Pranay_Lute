86a7d043cd7884a79436a532ccb24d5f942789f7
Pranaylute19 <pranaylute1@gmail.com>
2023-10-09 11:23:11 +0530

    Beacon HCS problem Solving

---
+++ JAVA_Programming/BEACON_HCS/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int feesForBelowAgeOf_17 = 300;
+        int feesBetweenAgeOf17To40 = 400;
+        int feesForAgeAbove_40 =200;
+        int totalFeesOfTheDay=0;
+
+        int[] age=new int[10];
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int counter=0;counter<age.length;counter++){
+            System.out.println("Enter age for patient "+counter+ " = ");
+            int PatientAge=scanner.nextInt();
+
+            if (PatientAge<=17){
+                System.out.println("fee : 300");
+                totalFeesOfTheDay+=feesForBelowAgeOf_17;
+            }if (PatientAge>=17 && PatientAge<=40){
+                System.out.println("fee : 400");
+                totalFeesOfTheDay+=feesBetweenAgeOf17To40;
+            }if (PatientAge>40){
+                System.out.println("fee : 200");
+                totalFeesOfTheDay+=feesForAgeAbove_40;
+            }
+            if(PatientAge>=120 ){
+                System.out.println("Invalid age input");
+            }
+        }
+        System.out.println("Total earning of the day = "+totalFeesOfTheDay);
+    }
+}
