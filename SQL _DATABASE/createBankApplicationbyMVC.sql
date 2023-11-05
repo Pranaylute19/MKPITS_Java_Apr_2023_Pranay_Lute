3498d194bbec45a0e29a9745a6cd9db5c616f188
Pranaylute19 <pranaylute1@gmail.com>
2023-11-06 00:06:12 +0530

    bank application with sql queries and create database

---
+++ SQL _DATABASE/createBankApplicationbyMVC.sql
@@ -0,0 +1,12 @@
+create database BankApplicationMVC
+use BankApplicationMVC
+
+create table customer(
+	User_id varchar(20),
+     password varchar(20),
+    name varchar(20),
+    Address varchar(20),
+    City varchar(20)
+)
+
+select * from customer
