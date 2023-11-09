a0005a99fbbba8d414b36dda06116b7c41e2a163
Pranaylute19 <pranaylute1@gmail.com>
2023-11-09 12:36:14 +0530

    bank appplication Admin Format

---
+++ JAVA_Programming/BankApplication/AdminEntity.java
@@ -0,0 +1,25 @@
+package com.example.bankapplicationbyusing_jsp;
+
+public class AdminEntity {
+    private String adminUsername;
+    private String adminPassword;
+
+    public AdminEntity() {
+    }
+
+    public String getAdminUsername() {
+        return adminUsername;
+    }
+
+    public void setAdminUsername(String adminUsername) {
+        this.adminUsername = adminUsername;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+}
