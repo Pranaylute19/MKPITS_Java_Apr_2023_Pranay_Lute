4df3c85fdaf7f58fec74313fd32a0c0d847c3c68
Pranaylute19 <pranaylute1@gmail.com>
2023-11-28 13:12:02 +0530

    Off desk project Application database

---
+++ OFF_DESK_APPLICATION_PROJECT/Database/create_database.sql
@@ -0,0 +1,11 @@
+create database OffDeskApplication
+use OffDeskApplication
+create table LoginPage(
+id varchar(15),
+password varchar(15))
+
+create table Registration(
+name varchar(10),
+designation varchar(20),
+Date_Of_joining date,
+gender varchar(10))
